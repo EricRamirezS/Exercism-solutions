1a0613eb58421ac406ffc3fde5464ff9c28d5977
EricRamirezs <eramirezsantis@gmail.com>
2019-05-29 02:12:56 -0400

    Update Hamming.java

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,34 +1,57 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Hamming {
 
-    private char[] leftStrand;
-    private char[] rightStrand;
+    private final String leftStrand;
+    private final String rightStrand;
+    private final int hammingDistance;
 
     Hamming(String leftStrand, String rightStrand) throws IllegalArgumentException {
-        if (isNullOrEmpty(leftStrand) ^ isNullOrEmpty(rightStrand)) {
-            if (isNullOrEmpty(leftStrand)) {
-                throw new IllegalArgumentException("left strand must not be empty.");
-            }
-            if (isNullOrEmpty(rightStrand)) {
-                throw new IllegalArgumentException("right strand must not be empty.");
-            }
-        }
-        if (leftStrand.length() != rightStrand.length()) {
-            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
-        }
-        this.leftStrand = leftStrand.toCharArray();
-        this.rightStrand = rightStrand.toCharArray();
+        validateInputs(leftStrand, rightStrand);
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
+        this.hammingDistance = calculateHammingDistance();
+    }
+
+    String getLeftStrand() {
+        return leftStrand;
+    }
+
+    String getRightStrand() {
+        return rightStrand;
     }
 
     int getHammingDistance() {
+        return hammingDistance;
+    }
+
+    private int calculateHammingDistance() {
         int hammingDistance = 0;
-        for (int i = 0; i < leftStrand.length; i++) {
-            hammingDistance += leftStrand[i] == rightStrand[i] ? 0 : 1;
+        for (int i = 0; i < leftStrand.length(); i++) {
+            hammingDistance += leftStrand.charAt(i) == rightStrand.charAt(i) ? 0 : 1;
         }
         return hammingDistance;
     }
 
-    boolean isNullOrEmpty(String str) {
+    private boolean isNullOrEmpty(String str) {
         return str == null || str.trim().isEmpty();
     }
+
+    private void validateInputs(String left, String right) throws IllegalArgumentException {
+        if (isNullOrEmpty(left) ^ isNullOrEmpty(right)) {
+            if (isNullOrEmpty(left)) {
+                throw new IllegalArgumentException("left strand must not be empty.");
+            }
+            if (isNullOrEmpty(right)) {
+                throw new IllegalArgumentException("right strand must not be empty.");
+            }
+        }
+        if (left.length() != right.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+    }
+
 
 }
